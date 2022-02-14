83008e81ba4eae319b6f48a4b236dad0258e2b2d
Kelvi <82715868+kelvi919@users.noreply.github.com>
2022-02-14 20:33:29 +0100

    Update google google.py

--- google google.py
+++ google google.py
@@ -3,7 +3,7 @@ import webbrowser
 
 def button_click():
     """A function that handles the opening of the browser"""
-    google_url = "https://www.google.com/search?q=what+happens+if+i+google+google&rlz=1C1CHBD_deDE975DE975&sxsrf=APq-WBt-2OM4liygKi-1KT30481AV4N3wQ%3A1644866162363&ei=cqoKYrXjFaar9u8PztaO2AQ&ved=0ahUKEwi15L368__1AhWmlf0HHU6rA0sQ4dUDCA8&uact=5&oq=what+happens+if+i+google+google&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEIAEMgYIABAWEB4yBggAEBYQHjoHCAAQRxCwAzoICAAQgAQQsAM6BAgjECdKBQg8EgExSgQIQRgASgQIRhgAUKYGWNAVYLUWaAFwAXgAgAFZiAGPBJIBATeYAQCgAQHIAQnAAQE&sclient=gws-wiz"
+    google_url = "http://www.google.com"
 
     # use your chrome file location for this to work
     webbrowser.open_new_tab(google_url)
